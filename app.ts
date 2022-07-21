d7b7756b6b65871b74d36cb205d194391643cd54
Nathan Froese <nathan.froese21@gmail.com>
2022-07-20 22:37:12 -0600

    For private and public

--- app.ts
+++ app.ts
@@ -3,15 +3,34 @@ class department {
     this.name = n;
   }
   name: string;
+  private employees: string[] = [];
+
   describe(this: department) {
     console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new department("Accounting");
 
+accounting.addEmployee("Bar");
+accounting.addEmployee("Foo");
+
+// // this will careate and error if employees is private making in unaccesable unless within the class
+// accounting.employees[2] = "BAHAH...";
+
 accounting.describe();
+accounting.printEmployeeInfo();
+// accounting.describe();
 
-const acctCopy = { name: "hello", describe: accounting.describe };
+// const acctCopy = { name: "hello", describe: accounting.describe };
 
-acctCopy.describe();
+// acctCopy.describe();
