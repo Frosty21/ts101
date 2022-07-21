7fa58a10a49294a8dedff9f7575660d073ae7c62
Nathan Froese <nathan.froese21@gmail.com>
2022-07-21 12:29:53 -0600

    getters, setters protected

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 class department {
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
   }
-  name: string;
-  private employees: string[] = [];
+  //   name: string;
+  protected employees: string[] = [];
 
   describe(this: department) {
     console.log("Department: " + this.name);
@@ -18,17 +18,65 @@ class department {
     console.log(this.employees);
   }
 }
+class AccountingDepartment extends department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+  private lastReport: string;
 
-const accounting = new department("Accounting");
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
 
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  addEmployee(name: string) {
+    if (name === "nath") {
+      return;
+    }
+    this.employees.push(name);
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+class ITDepartment extends department {
+  admins: string[];
+  constructor(id: string, admins: string[], private reports: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+const accounting = new AccountingDepartment("foo", []);
+
+accounting.addEmployee("nath");
 accounting.addEmployee("Bar");
 accounting.addEmployee("Foo");
+accounting.printEmployeeInfo();
 
+accounting.mostRecentReport = "YER";
+console.log(accounting.mostRecentReport);
+accounting.addReport("Some report");
+accounting.printReports();
 // // this will careate and error if employees is private making in unaccesable unless within the class
 // accounting.employees[2] = "BAHAH...";
 
-accounting.describe();
-accounting.printEmployeeInfo();
+// accounting.describe();
+// accounting.printEmployeeInfo();
 // accounting.describe();
 
 // const acctCopy = { name: "hello", describe: accounting.describe };
